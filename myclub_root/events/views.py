48de6995c6d08cee9a0fbc7f48e100f998e6551a
yairtele <ytelezon@est.frba.utn.edu.ar>
2023-09-19 13:00:29 +0200

    Addition of url with regex for month and year args and view with calendar and text showing month and year

--- myclub_root/events/views.py
+++ myclub_root/events/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
+import calendar
+from calendar import HTMLCalendar
 
-def index(request):
-    return HttpResponse("<h1>MyClub Event Calendar</h1>")
+def index(request, year=date.today().year, month=date.today().month):
+    t = date.today()
+    month = int(month)#date.strftime(t, '%b')
+    year = int(year)
+    if year < 2000 or year > 2099: year = date.today().year
+    month_name = calendar.month_name[month]
+    title = "MyClub Event Calendar - %s %s" % (month_name, year)
+    cal = HTMLCalendar().formatmonth(year, month)
+
+    return HttpResponse("<h1>%s</h1><p>%s</p>" % (title, cal))
