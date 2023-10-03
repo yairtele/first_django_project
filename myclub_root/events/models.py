30969e02a578f9e70e3397450fef53e157501e6a
yairtele <ytelezon@est.frba.utn.edu.ar>
2023-10-03 12:31:40 +0200

    Addition of custom method Event.event_timing() and Event.name_slug() also as property. Overriding of Event.save() method. Modification of VenueManager.

--- myclub_root/events/models.py
+++ myclub_root/events/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class VenueManager(models.Manager):
+    def get_queryset(self):
+        return super(VenueManager, self).get_queryset().filter(zip_code='00000')
+
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
@@ -8,6 +12,9 @@ class Venue(models.Model):
     phone = models.CharField('Contact Phone', max_length=20, blank=True)
     web = models.URLField('Web Address', blank=True)
     email_address = models.EmailField('Email Address', blank=True)
+
+    venues = models.Manager()
+    local_venues = VenueManager()
 
     def __str__(self):
        return self.name
@@ -21,6 +28,9 @@ class MyClubUser(models.Model):
     def __str__(self):
         return self.first_name + " " + self.last_name
 
+class EventManager(models.Manager):
+    def event_type_count(self, event_type):
+        return self.filter(name__icontains=event_type).count()
 
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
@@ -29,6 +39,23 @@ class Event(models.Model):
     manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
     description = models.TextField(blank=True)
+    events = EventManager()
 
+    def save(self, *args, **kwargs):
+        self.manager = User.objects.get(username='admin') # User 'admin' must exist
+        super(Event, self).save(*args, **kwargs)
+ 
+    def event_timing(self, date):
+        if self.event_date > date:
+            return "Event is after this date"
+        elif self.event_date == date:
+            return "Event is on the same day"
+        else:
+            return "Event is before this date"
+ 
+    @property
+    def name_slug(self):
+        return self.name.lower().replace(' ','-')
+ 
     def __str__(self):
         return self.name
